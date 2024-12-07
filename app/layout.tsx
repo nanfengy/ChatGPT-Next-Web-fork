a5f6290080d3ee3dc18b31fd66bbebcd33ea0bdd
liujinkang <290288572@qq.com>
2024-12-07 19:38:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "物理AI实验室",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "物理AI实验室",
     statusBarStyle: "default",
   },
 };
